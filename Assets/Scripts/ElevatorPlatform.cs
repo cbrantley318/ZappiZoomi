689a62a077cb2b00299bb3045a9eb0add4d2500a
lathamhall <lathamhall04@gmail.com>
2025-11-16 13:06:21 -0500

    elevator pretty much good to go

--- Assets/Scripts/ElevatorPlatform.cs
+++ Assets/Scripts/ElevatorPlatform.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class ElevatorPlatform : MonoBehaviour
 {
     // Checkbox in the Inspector to turn the elevator on or off
-    public bool poweredOn = true;
+    public bool poweredOn = true; //obvi change this later to be on when powered on and off when not
 
     // elevator speed zoom zoom
-    public float speed = 2f;
+    public float speed; //rn it's 2
 
     // distance it moves up
-    public float moveDistance = 3f;
+    public float moveDistance; //rn it's 3
 
     // start position
     private Vector3 startPosition;
@@ -21,11 +21,10 @@ public class ElevatorPlatform : MonoBehaviour
 
     void Update()
     {
-        // If not powered, do nothing
+        
         if (!poweredOn)
             return;
 
-        
         float offset = Mathf.PingPong(Time.time * speed, moveDistance); // PingPong is an excellent function name btw
         float newY = startPosition.y + offset;
         transform.position = new Vector3(startPosition.x, newY, startPosition.z);
