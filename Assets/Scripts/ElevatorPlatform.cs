75b9bfad7536b3e652f8905d39d1cb0dc6b21931
Carson Brantley <carson.brantley@duke.edu>
2025-11-28 19:21:59 -0500

    major elevator revamp

--- Assets/Scripts/ElevatorPlatform.cs
+++ Assets/Scripts/ElevatorPlatform.cs
@@ -1,4 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class ElevatorPlatform : MonoBehaviour
 {
@@ -11,9 +14,22 @@ public class ElevatorPlatform : MonoBehaviour
     // distance it moves up
     public float moveDistance; //rn it's 3
 
-    // start position
+    // is it an elevator (true) or a gate(false)
+    [SerializeField] bool pingpong;     //what a great variable name
+
+    // position state information
     private Vector3 startPosition;
     private Rigidbody2D MyRigidBody;
+    private LineRenderer MyLineRenderer;
+
+    private List<Vector3> path;
+    private bool goingUp = true;    //start out incrementing path, when you hit the top pause and then go down, then go back
+    private float pauseTime;
+    private float pauseInterval = 0.2f; //pause duration (in seconds!)
+    private int currPos = 0;
+
+    private int moveDirection = 1;  //1 is up, 0 is back (dont edit this even though it's pretty much identical to goingUp
+    private GameObject parentObj;
 
     //TODO: multiple possible changes (listed below, all optional so idk):
     /*
@@ -24,54 +40,159 @@ public class ElevatorPlatform : MonoBehaviour
      * Add an InstantDeathZone Trigger below the elevator to kill the player (maybe don't make this part of the prefab, just for the one instance)
      */
 
-    private int moveDirection = 1;  //1 is up, 0 is back
-    private GameObject parentObj;
+
+    private void Awake()
+    {
+        parentObj = transform.parent.gameObject;
+        MyRigidBody = GetComponent<Rigidbody2D>();
+        MyLineRenderer = parentObj.GetComponent<LineRenderer>();
+
+        startPosition = transform.position;
+
+        //fill the path with the linerender points, then the elevator will move point-to-point
+        path = new();
+        for (int i = 0; i < MyLineRenderer.positionCount; i++)
+        {
+            path.Add(MyLineRenderer.GetPosition(i) + transform.position);   //for now applying the world->local offset here
+        }
+        MyLineRenderer.enabled = false; //make it disappear //optional todo: make it appear to the player if it's aesthetically pleasing (think mario 64 elevator paths)
+        MyRigidBody.MovePosition(path[0]);
+
+    }
 
 
     void Start()
     {
-        startPosition = transform.position;
-        MyRigidBody = GetComponent<Rigidbody2D>();
-        parentObj = transform.parent.gameObject;
+        goingUp = true;
     }
 
     void Update()
     {
         poweredOn = parentObj.GetComponentInChildren<PowerTermScript>().poweredOn;  //there can be only one (PowerTermScript, that is)
-                
 
-        float offset = Mathf.PingPong(Time.time * speed, moveDistance); // PingPong is an excellent function name btw   //wholeheartedly agree
-        float newY = startPosition.y + offset;
+        //float offset = Mathf.PingPong(Time.time * speed, moveDistance); // PingPong is an excellent function name btw   //wholeheartedly agree
+        //float newY = startPosition.y + offset;
 
         //transform.position = new Vector3(startPosition.x, newY, startPosition.z);
         if (poweredOn)
         {
-            if (newY > transform.position.y)                        //changed to this becase the RigidBody stuff doesn't like editing the transform.position directly :(
-            {                                                       //also because the playerScript needs to get the velocity of this, so for that to work we need a velocity
-                MyRigidBody.velocity = new Vector2(0, speed);
-                moveDirection = 1;
-            } else if (newY < transform.position.y)
-            {
-                MyRigidBody.velocity = new Vector2(0, -speed);
-                if (moveDirection == 1 && GameObject.FindWithTag("Player").transform.IsChildOf(transform))
+            //move to the next position
+
+            //if (Time.time - pauseTime > pauseInterval)  //if 
+            //{
+                //allow movement once we've paused long enough
+                if (goingUp)
                 {
-                    GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().velocity = MyRigidBody.velocity;
+                    MoveUp(pingpong);   //handles the logic for incrementing state and telling us when to move
+                } else
+                {
+                    MoveDown(pingpong);
                 }
-                moveDirection = 0;
+        } else
+        {
+            MoveDown(false);
+        }
+    }
+
+    void MoveUp(bool continueMotion)
+    {
+        Assert.IsTrue(poweredOn);//sanity checking
+
+        //if we havent reach the current target point, then move to it
+        //if we have, then check if it's the last one or increment
+
+        if (currPos == 0)
+        {
+            currPos++;
+        }
+
+        if (HitTarget(path[currPos-1], path[currPos], MyRigidBody.transform.position))
+        {   
+
+            if (currPos == path.Count - 1)
+            {
+                if (continueMotion)     //if false, will always stay here (for the non-elevator methods
+                {
+                    goingUp = false;    //start moving elevator back down
+                    Debug.Log("Going Down");
+                }
+                MyRigidBody.velocity = Vector3.zero;   //stop moving and pause
+                pauseTime = Time.time;
+            }
+            else
+            {
+                currPos++;
             }
         } else
         {
-            //if poweredOff, move back to resting position
-            if (!Mathf.Approximately((MyRigidBody.transform.position - startPosition).magnitude, 0))
-            {
-                MyRigidBody.velocity = speed * (startPosition - transform.position).normalized;
-            } else
-            {
-                MyRigidBody.velocity = Vector3.zero;
-            }
+            Debug.Log("------------");
+            Debug.Log(MyRigidBody.transform.position);
+            Debug.Log(path[currPos]);
+            Debug.Log((MyRigidBody.transform.position - path[currPos]).magnitude);
+            Debug.Log("------------");
+
+            MyRigidBody.velocity = speed * (path[currPos] - transform.position).normalized;   //path[0] is the startPosition now
         }
 
     }
 
+    void MoveDown(bool continueMotion)
+    {
+        if (currPos == path.Count -1)
+        {
+            currPos--;
+        }
+
+
+        if (HitTarget(path[currPos+1], path[currPos], MyRigidBody.transform.position))
+        {
+            if (currPos == 0)
+            {
+                if (continueMotion)     //if false, will always stay here (for the non-elevator methods
+                {
+                    goingUp = true;    //start moving elevator back down
+                    Debug.Log("Going Up");
+                }
+                MyRigidBody.velocity = Vector3.zero;   //stop moving and pause
+                pauseTime = Time.time;
+            }
+            else
+            {
+                currPos--;
+            }
+        }
+        else
+        {
+            MyRigidBody.velocity = speed * (path[currPos] - transform.position).normalized;   //path[0] is the startPosition now
+        }
+
+    }
+    
+
+    //------- HELPERS FOR FIXING UNITY'S FLOATING-POINT MATH (their threshold is wayyyy too low for this ----------//
+
+    bool MyFloatApprox(float a, float b)
+    {
+        return MyFloatApprox(a, b, 0.0001f);
+    }
+
+    bool MyFloatApprox(float a, float b, float thresh)      //ended up not 
+    {
+        return (Mathf.Abs(a - b) < thresh);
+    }
+
+    bool HitTarget(Vector3 start, Vector3 goal, Vector3 curr)
+    {
+        // so now we need to check where we are with direction AND magnitude!
+
+        if (MyFloatApprox((MyRigidBody.transform.position - path[currPos]).magnitude, 0))   //first check if we're close enough
+            return true;
+
+        //now check if we overshot (stupid floating-point precision ugh)
+        if (Vector3.Dot((curr - goal), (start - goal)) < 0)                 //just draw the triangles
+            return true;
+
+        return false;
+    }
 
 }
